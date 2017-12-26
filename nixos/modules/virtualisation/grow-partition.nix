6be61a91f9d2d428d6e6bc88e61752324d5d95a9
Benjamin Smith <polynomial@gmail.com>
2017-12-26 12:14:15 -0800

    hack to fix nvme partition numbers

--- nixos/modules/virtualisation/grow-partition.nix
+++ nixos/modules/virtualisation/grow-partition.nix
@@ -40,7 +40,13 @@ with lib;
       if [ -e "$rootDevice" ]; then
         rootDevice="$(readlink -f "$rootDevice")"
         parentDevice="$(lsblk -npo PKNAME "$rootDevice")"
-        TMPDIR=/run sh $(type -P growpart) "$parentDevice" "''${rootDevice#$parentDevice}"
+        # support nvme style partitions on AWS
+        if echo "$parentDevice" | grep nvme >/dev/null 2>&1; then
+          partitionNumber="''${rootDevice#${parentDevice}p}"
+        else
+          partitionNumber="''${rootDevice#$parentDevice}"
+        fi
+        TMPDIR=/run sh $(type -P growpart) "$parentDevice" "$partitionNumber"
         udevadm settle
       fi
     '';
