3e4e851419ef7337a53f55410ff3d90ad072036b
sergeykranga <sergey.kranga@gmail.com>
2025-01-13 11:56:37 +0200

    no more changes to promtail

--- nixos/modules/services/logging/promtail.nix
+++ nixos/modules/services/logging/promtail.nix
@@ -1,24 +1,46 @@
 { config, lib, pkgs, ... }: with lib;
 let
   cfg = config.services.promtail;
-in
-{
+
+  prettyJSON = conf: pkgs.runCommandLocal "promtail-config.json" {} ''
+    echo '${builtins.toJSON conf}' | ${pkgs.buildPackages.jq}/bin/jq 'del(._module)' > $out
+  '';
+
+  allowSystemdJournal = cfg.configuration ? scrape_configs && lib.any (v: v ? journal) cfg.configuration.scrape_configs;
+
+  allowPositionsFile = !lib.hasPrefix "/var/cache/promtail" positionsFile;
+  positionsFile = cfg.configuration.positions.filename;
+
+  configFile = if cfg.configFile != null
+                   then cfg.configFile
+                   else prettyJSON cfg.configuration;
+
+in {
   options.services.promtail = with types; {
-    enable = mkEnableOption (lib.mdDoc "the Promtail ingresser");
+    enable = mkEnableOption "the Promtail ingresser";
+
+    configuration = mkOption {
+      type = (pkgs.formats.json {}).type;
+      description = ''
+        Specify the configuration for Promtail in Nix.
+        This option will be ignored if `services.promtail.configFile` is defined.
+      '';
+    };
 
     configFile = mkOption {
-      type = types.nullOr types.path;
+      type = nullOr path;
       default = null;
-      description = lib.mdDoc ''
-        Specify a configuration file that Loki should use.
+      description = ''
+        Config file path for Promtail.
+        If this option is defined, the value of `services.promtail.configuration` will be ignored.
       '';
     };
 
     extraFlags = mkOption {
       type = listOf str;
-      default = [ ];
+      default = [];
       example = [ "--server.http-listen-port=3101" ];
-      description = lib.mdDoc ''
+      description = ''
         Specify a list of additional command line flags,
         which get escaped and are then passed to Loki.
       '';
@@ -26,16 +48,22 @@ in
   };
 
   config = mkIf cfg.enable {
+    services.promtail.configuration.positions.filename = mkDefault "/var/cache/promtail/positions.yaml";
+
     systemd.services.promtail = {
       description = "Promtail log ingress";
       wantedBy = [ "multi-user.target" ];
       stopIfChanged = false;
 
+      preStart = ''
+        ${lib.getExe pkgs.promtail} -config.file=${configFile} -check-syntax
+      '';
+
       serviceConfig = {
         Restart = "on-failure";
         TimeoutStopSec = 10;
 
-        ExecStart = "${pkgs.promtail}/bin/promtail -config.file=${cfg.configFile} ${escapeShellArgs cfg.extraFlags}";
+        ExecStart = "${pkgs.promtail}/bin/promtail -config.file=${configFile} ${escapeShellArgs cfg.extraFlags}";
 
         ProtectSystem = "strict";
         ProtectHome = true;
@@ -46,6 +74,7 @@ in
         RestrictSUIDSGID = true;
         PrivateMounts = true;
         CacheDirectory = "promtail";
+        ReadWritePaths = lib.optional allowPositionsFile (builtins.dirOf positionsFile);
 
         User = "promtail";
         Group = "promtail";
@@ -63,13 +92,14 @@ in
         RestrictRealtime = true;
         MemoryDenyWriteExecute = true;
         PrivateUsers = true;
-      } // (optionalAttrs (!pkgs.stdenv.isAarch64) {
-        # FIXME: figure out why this breaks on aarch64
+
+        SupplementaryGroups = lib.optional (allowSystemdJournal) "systemd-journal";
+      } // (optionalAttrs (!pkgs.stdenv.hostPlatform.isAarch64) { # FIXME: figure out why this breaks on aarch64
         SystemCallFilter = "@system-service";
       });
     };
 
-    users.groups.promtail = { };
+    users.groups.promtail = {};
     users.users.promtail = {
       description = "Promtail service user";
       isSystemUser = true;
